0b22e72e9dbbfaf2bbc0b00c5888b6c3c1360d57
dpizapal <dpizapal@yahoo.es>
2022-06-17 20:38:37 +0000

    ???

--- src/app.js
+++ src/app.js
@@ -10,48 +10,49 @@ window.onload = () => {
   document.querySelector("#btn").addEventListener("click", () => {
     document.querySelector("#the-excuse").innerHTML = generateExcuse();
   });
+
+  let generateExcuse = () => {
+    let who = [
+      "The dog",
+      "My granma",
+      "A vulture",
+      "My bird",
+      "My little brother",
+      "A comedian"
+    ];
+
+    let action = ["ate", "pissed", "crushed", "broked", "took", "stole"];
+
+    let posseion = [
+      "my homework",
+      "the car",
+      "my keys",
+      "the computer",
+      "my umbrella"
+    ];
+
+    let where = [
+      "during my breakfast",
+      "in my house",
+      "lastnight",
+      "on the street",
+      "at work"
+    ];
+
+    let whoindx = Math.floor(Math.random() * who.length);
+    let actionindx = Math.floor(Math.random() * action.length);
+    let possindx = Math.floor(Math.random() * posseion.length);
+    let whereindx = Math.floor(Math.random() * where.length);
+
+    return (
+      who[whoindx] +
+      " " +
+      action[actionindx] +
+      " " +
+      posseion[possindx] +
+      " " +
+      where[whereindx]
+    );
+  };
+  console.log(generateExcuse);
 };
-let generateExcuse = () => {
-  let who = [
-    "The dog",
-    "My granma",
-    "A vulture",
-    "My bird",
-    "My little brother",
-    "A comedian"
-  ];
-
-  let action = ["ate", "pissed", "crushed", "broked", "took", "stole"];
-
-  let posseion = [
-    "my homework",
-    "the car",
-    "my keys",
-    "the computer",
-    "my umbrella"
-  ];
-
-  let where = [
-    "during my breakfast",
-    "in my house",
-    "lastnight",
-    "on the street",
-    "at work"
-  ];
-
-  let whoindx = Math.floor(Math.random() * who.length);
-  let actionindx = Math.floor(Math.random() * action.length);
-  let possindx = Math.floor(Math.random() * posseion.length);
-  let whereindx = Math.floor(Math.random() * where.length);
-
-  return (
-    who[whoindx] +
-    " " +
-    action[actionindx] +
-    " " +
-    posseion[possindx] +
-    " " +
-    where[whereindx]
-  );
-};
-console.log(generateExcuse);
